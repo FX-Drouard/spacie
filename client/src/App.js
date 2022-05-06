de2ed4c9b2afcc6675056852cdaf01ba4ea402a0
Fristorm <fristorm@spacie.fr>
2022-05-06 11:58:43 +0200

    fix serv

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
     super(props);
     this.setBody = this.setBody.bind(this);
     // const host = location.hostname;
-    axios.defaults.baseURL = 'http://spacie.fr/';
+    axios.defaults.baseURL = 'https://spacie.fr/';
   
     this.state = {
       page: null,
